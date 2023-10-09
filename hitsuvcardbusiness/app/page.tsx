e452c4369caa1966fd98b9b9099ee89160539676
Hitsukaya Valentaizar <87619517+Hitsukaya@users.noreply.github.com>
2023-10-09 11:11:42 +0300

    Add files via upload

---
+++ hitsuvcardbusiness/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Heroone from './components/widgets/Heroone';
+import FourElements from './components/widgets/FourElements';
+import InfoAlert from './components/widgets/InfoAlert';
+export default function Home() {
+  return (
+
+     <><Heroone />
+     
+     <InfoAlert />
+
+     <FourElements /></>
+
+  );
+}
